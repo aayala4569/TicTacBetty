1f9d5334f3ccb4d52a232dac0b0252090f99f8a2
aayala4569 <angelicaayala100@gmail.com>
2023-11-08 19:38:30 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Game from "./Components/Game";
+import Images from "../images/BG.jpeg";
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${Images})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "800px",
+        width: "100%",
+      }}
+    >
+      <Game />
+    </div>
+  );
+}
+
+export default App;
